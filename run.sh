32e4120c4ca1a1fe236b4d8e103452c53f0f0996
Salvador Salcedo <salsalcedo4321@gmail.com>
2022-09-14 14:12:46 -0700

    created our own chainlink fork

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 echo "Running script"
 
 service1="cd /Users/alex/Iris/chainlink && ./chainlink node start"
+service2="cd /Users/alex/Iris/iris-external-adapter && python app.py"
 
 osascript -e "tell application \"Terminal\" to do script \"${service1}\""
+osascript -e "tell application \"Terminal\" to do script \"${service2}\""
 
 #./chainlink node start && python /iris-external-adapter app.py
